63d7753b827860fc5fa21bc2e18cfad85453e453
Gabriel Martínez Llano <gmecatronics@gmail.com>
2023-06-28 08:04:25 -0500

    Changing comments from Spanish to English.

--- Text-Parser.py
+++ Text-Parser.py
@@ -1,30 +1,29 @@
 import tparser
 
-#Fase 1: Abrir el archivo
-showtech = open("Archivos de Prueba/ANB-RTR-WAN-1 sh tech.txt","r") #Apretura del arhivo en modo de lectura
-parsed = open("Parsed Show Tech.txt","w+") #Apertura del archivo de salida. como este archivo no existe, entonces
-#este archivo será creado de antemano. w+ significa que el archivo está en modo de escritura (w) y lectura (+)
+#Phase 1:Open the file
+showtech = open("Archivos de Prueba/ANB-RTR-WAN-1 sh tech.txt","r") #Opening the file in reading mode
+parsed = open("Parsed Show Tech.txt","w+") #Opening the output file. as the file does not exist, then it is created
+#w+ mean the file is in writing (w) and reading mode (+).
 
-#Fase 2: Obtener las sub-cadenas usando regex
-result = tparser.filter_text(showtech) #Regresa una lista de sub-cadenas que se quieren escribir en el archivo de salida
+#Fase 2: Get sub-strings using regex
+result = tparser.filter_text(showtech) #Returna a list of sub-strings with the desired text snipppets
 
 
-#Fase 3: Grabar las sub-cadenas en un archivo de texto
+#Fase 3: Save sub-strings on a text file
 tparser.writer(result,parsed)
 
-#Fase 4: Analizar
+#Fase 4: Analyze
 parsed.close() #Cerrar el archivo es necesario para abrir el archivo con lo cambios escritos en el la fase 3. Esto reuire investigación#Closing the file is necesary to open the file with the changes. This requires more research
-parsed = open("Parsed Show Tech.txt","r+") #Abriendo archivo en modo de lectura (r) y escritura (+s)
-text = parsed.read() #Leyendo archivo modificado
-version = tparser.searchVersion(text) #Obteniendo la versión del sistema operativo presente en el archivo de salida
-firmware = tparser.searchROMMON(text) #Obteniendo la versión de la ROM presente en el archivo de salida
-parsed.write("\n\n")  #No se puede usar tparser.writer() porque fue diseñado para una lista de sub-cadenas
-#de caracteres
+parsed = open("Parsed Show Tech.txt","r+") #Opening the file in reading (r) and writing mode (+)
+text = parsed.read() #Reading output file
+version = tparser.searchVersion(text) #Getting OS version
+firmware = tparser.searchROMMON(text) #Getting ROM version
+parsed.write("\n\n")  #tparser.writer() won't work becase that method will work to print the snippet lists, not a single text line.
 parsed.write("---------- Observaciones ----------")
-parsed.write("\n\n") #el métoo write() en un objeto tipo archivo escribe una línea de texto en un archivo.
+parsed.write("\n\n") #write() method prints a text line on a file object.
 parsed.write("\n\n")
     
-#Fase 5: Cerrar objetos tipo archivos para limpiar el búffer del systema 
+#Fase 5: Close files to clean system buffer 
 showtech.close()
 parsed.close()
 print("Rutina finalizada con exito")
