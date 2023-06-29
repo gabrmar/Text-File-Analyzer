5680ed88f4d5bfeebb98c5f54404b5159d24ffc6
Gabriel Martínez Llano <gmecatronics@gmail.com>
2023-06-29 10:24:16 -0500

    renaming file names.

--- Text-Analyzer.py
+++ Text-Analyzer.py
@@ -1,4 +1,4 @@
-import tparser
+import tfanalyzer
 
 #Phase 1:Open the file
 showtech = open("Archivos de Prueba/ANB-RTR-WAN-1 sh tech.txt","r") #Opening the file in reading mode
@@ -6,19 +6,19 @@ parsed = open("Parsed Show Tech.txt","w+") #Opening the output file. as the file
 #w+ mean the file is in writing (w) and reading mode (+).
 
 #Fase 2: Get sub-strings using regex
-result = tparser.filter_text(showtech) #Returna a list of sub-strings with the desired text snipppets
+result = tfanalyzer.filter_text(showtech) #Returna a list of sub-strings with the desired text snipppets
 
 
 #Fase 3: Save sub-strings on a text file
-tparser.writer(result,parsed)
+tfanalyzer.writer(result,parsed)
 
 #Fase 4: Analyze
 parsed.close() #Cerrar el archivo es necesario para abrir el archivo con lo cambios escritos en el la fase 3. Esto reuire investigación#Closing the file is necesary to open the file with the changes. This requires more research
 parsed = open("Parsed Show Tech.txt","r+") #Opening the file in reading (r) and writing mode (+)
 text = parsed.read() #Reading output file
-version = tparser.searchVersion(text) #Getting OS version
-firmware = tparser.searchROMMON(text) #Getting ROM version
-parsed.write("\n\n")  #tparser.writer() won't work becase that method will work to print the snippet lists, not a single text line.
+version = tfanalyzer.searchVersion(text) #Getting OS version
+firmware = tfanalyzer.searchROMMON(text) #Getting ROM version
+parsed.write("\n\n")  #tfanalyzer.writer() won't work becase that method will work to print the snippet lists, not a single text line.
 parsed.write("---------- Observaciones ----------")
 parsed.write("\n\n") #write() method prints a text line on a file object.
 parsed.write("\n\n")
