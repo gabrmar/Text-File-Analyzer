f3f5cd1aca661ea903321ba39c34d24b5f01cb05
Gabriel Martínez Llano <gmecatronics@gmail.com>
2023-07-03 20:37:36 -0500

    Finishing translation of comments.

--- Text-Analyzer.py
+++ Text-Analyzer.py
@@ -5,15 +5,15 @@ showtech = open("Archivos de Prueba/ANB-RTR-WAN-1 sh tech.txt","r") #Opening the
 parsed = open("Parsed Show Tech.txt","w+") #Opening the output file. as the file does not exist, then it is created
 #w+ mean the file is in writing (w) and reading mode (+).
 
-#Fase 2: Get sub-strings using regex
+#Phase 2: Get sub-strings using regex
 result = tfanalyzer.filter_text(showtech) #Returna a list of sub-strings with the desired text snipppets
 
 
-#Fase 3: Save sub-strings on a text file
+#Phase 3: Save sub-strings on a text file
 tfanalyzer.writer(result,parsed)
 
-#Fase 4: Analyze
-parsed.close() #Cerrar el archivo es necesario para abrir el archivo con lo cambios escritos en el la fase 3. Esto reuire investigación#Closing the file is necesary to open the file with the changes. This requires more research
+#Phase 4: Analyze
+parsed.close() #CClosing the file is necesary to open the file with the changes. This requires more research
 parsed = open("Parsed Show Tech.txt","r+") #Opening the file in reading (r) and writing mode (+)
 text = parsed.read() #Reading output file
 version = tfanalyzer.searchVersion(text) #Getting OS version
